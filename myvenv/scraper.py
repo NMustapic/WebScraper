28051c52bee559d8046329c0f90f5fb82e7a0dfe
Natan <nmustapic95@gmail.com>
2022-03-02 15:32:59 +0100

    Started work on conditions for printing links and titles

--- myvenv/scraper.py
+++ myvenv/scraper.py
@@ -1,3 +1,4 @@
+from cmath import nan
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -8,9 +9,9 @@ soup = BeautifulSoup(html, features="lxml")
 
 # print(soup)
 
-f = open('index.html', 'wb')
+'''f = open('index.html', 'wb')
 f.write(soup.encode("utf-8"))
-f.close #zapisivamo cjelokupni HTML kod stranice u .html datoteku
+f.close''' #zapisivamo cjelokupni HTML kod stranice u .html datoteku
 
 #mydivs = soup.find_all("a", {"class": "vijesti-text-hover"})
 #print(mydivs) #ispisiva html kod najvaznijih vijesti
@@ -25,9 +26,14 @@ for link in soup.find_all('a', href=True):
 br=0
 for element in nizlinkova:
     if element[0:4]=="http":
-        print("------------------------------------------")
-        print(niznaslova[br])
-        print(element) #svi elementi niza su linkovi sa stranice
+        ispisivanje = True
+        for rijec in niznaslova[br]:
+            if rijec[-1] =="€" or rijec == "Instagram" or rijec=="Facebook" or rijec=="Oglašavanje" or rijec=="recept":
+                ispisivanje = False #bolje pogledati petlju i popraviti uvjete ispisivanja
+        if ispisivanje:
+            print("------------------------------------------")
+            print(niznaslova[br])
+            print(element) #svi elementi niza su linkovi sa stranice 
     br+=1
         
 
